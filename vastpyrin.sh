dcc94301330484fe5ac93ff985081ee9e20153a6
dodgedaemon <129219931+dodgedaemon@users.noreply.github.com>
2024-01-02 21:03:53 +0100

    Update vastpyrin.sh

--- vastpyrin.sh
+++ vastpyrin.sh
@@ -18,13 +18,13 @@ sudo apt -y install screen  > /dev/null 2>&1
 echo "STEP 4 of 9: Installed Screen"
 
 echo "STEP 5 of 9: Downloading lolMiner"
-sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.82/lolMiner_v1.82_Lin64.tar.gz > /dev/null 2>&1
+sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.82a/lolMiner_v1.82a_Lin64.tar.gz > /dev/null 2>&1
 
 echo "STEP 6 of 9: Extracting lolMiner"
-sudo tar -xvf lolMiner_v1.82_Lin64.tar.gz > /dev/null 2>&1
+sudo tar -xvf lolMiner_v1.82a_Lin64.tar.gz > /dev/null 2>&1
 
 echo "STEP 7 of 9: Changing Directory"
-cd 1.82
+cd 1.82a
 
 echo "STEP 8 of 9: Updating Wallet Address in mine_pyrin.sh"
 sudo sed -i 's/WALLET=pyrin:qq92h3nryfwq0gkh73cwvjh9hhqlq2mank9sfxtgc99hqwn2ec6u2gszphr0u/WALLET=pyrin:qqyuygj8tla5nh3rdqhhed8kht3362aj0n9zrvk0y5wgj70ldp8qgwp25eugf.lolMinerWorkerVast/g' mine_aleph.sh > /dev/null 2>&1
