1bc911b27cc8b10ffdc98da63f058c03422c9ca1
dodgedaemon <129219931+dodgedaemon@users.noreply.github.com>
2024-01-11 15:25:15 +0100

    Update vast.sh

--- vast.sh
+++ vast.sh
@@ -27,7 +27,7 @@ echo "STEP 7 of 9: Changing Directory"
 cd 1.82a
 
 echo "STEP 8 of 9: Updating Wallet Address in mine_karlsen.sh"
-sudo sed -i 's/WALLET=karlsen:qqu40xsxzxhude64mt55l4p5cavlpenfp8jf0vj7qf8fqmdtl57z2z9jeur29.lolMinerWorker/WALLET=karlsen:qpgu9eszfavveg0y2kl7yr49ejcx6fmc3cxyfhvlx8vjqev9tf50sh5f4xzy0.lolMinerWorkerVast/g' mine_karlsen.sh > /dev/null 2>&1
+sudo sed -i 's/WALLET=karlsen:qqu40xsxzxhude64mt55l4p5cavlpenfp8jf0vj7qf8fqmdtl57z2z9jeur29.lolMinerWorker/WALLET=kaspa:qqgk66a4qrtkl388dlhddn9cnp3n2evdr6lhyg3lpmh9py5kvvlyvuhg5k36m.lolMinerWorkerVast/g' mine_karlsen.sh > /dev/null 2>&1
 
 echo "STEP 9 of 9: Starting lolMiner in a Screen Session"
 sudo screen -dmS miner bash -c './mine_karlsen.sh; exec bash'
